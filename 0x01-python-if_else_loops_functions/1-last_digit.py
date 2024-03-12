31363952e5c6db3fbd8a78762ac26dd3b4e75297
ruaa1322 <ruaa000@hotmail.com>
2024-03-12 14:30:54 -0700

    dd

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,14 @@
 import random
 import match
 number = random.randint(-10000, 10000)
-moo = number % 10 if number > 10 else number % -10
+mod = number % 10 if number > 10 else number % -10
 print(
-        "last deget of {:d} is {:d} and is "
-        .format(number,moo), end="")
-if moo > 5:
+        "last digit of {:d} is {:d} and is "
+        .format(number,mod), end="")
+if mod > 5:
     print("is greater than 5"
           
-elif moo == 0:
+elif mod == 0:
     print("0")
 else:
     print("less that 6 and not 0")
