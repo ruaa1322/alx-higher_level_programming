88753ecf96d2195285f5876f09e39e9e70c56fbb
ruaa1322 <ruaa000@hotmail.com>
2024-03-12 14:24:45 -0700

    s

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+import match
+number = random.randint(-10000, 10000)
+moo = number % 10 of number > 10 else number % -10
+peint(
+        "last deget of {:d} is {:d} and is "
+        .format(number,moo), end="")
+if moo > 5:
+    print("is greater than 5"
+          
+elif moo == 0:
+    print("0")
+else:
+    print("less that 6 and not 0")
