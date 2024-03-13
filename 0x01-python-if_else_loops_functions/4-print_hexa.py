2b9c56e1d1c2508f88e46caf5d5d2bf3276f675f
ruaa1322 <ruaa000@hotmail.com>
2024-03-13 14:10:33 -0700

    gg

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{} = {}".format(i, hex(i)))
+# 4-print_hexa.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+"""Print numbers 0 to 98 in decimal and hexadecimal."""
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+# 8-uppercase.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def uppercase(str):
-    for i in range(len(str)):
-        char = ord(str[i])
-        if char >= 97 and char <= 122:
-            char = char - 32
-        print("{}".format(chr(char)), end="")
-    print()
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
